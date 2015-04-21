cf1425b67daec1755a54f18a6bc4ee19a4f98f0a
hllwd <nicolasmondon@gmail.com>
2015-04-21 22:34:00 +0200

    - Retrieve name : add a corpus of the most frequent french words

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ http://julian.com/research/blast/
 * http://clair.si.umich.edu/~radev/papers/lprj.pdf
 * rita / natural
 * https://github.com/NaturalNode/natural
+* http://eduscol.education.fr/cid47916/liste-des-mots-classee-par-frequence-decroissante.html
+--> JSON.stringify($('.divTable').find('tr').map(function(){ return $(this).find('td').eq(1).text() }).toArray())   
--- retrieve-name.js
+++ retrieve-name.js
@@ -2,19 +2,31 @@
 
 var fs = require('fs')
 var path = require('path')
-var natural = require('natural')
-var tokenizer = new natural.WordTokenizer()
+var indexOf = require('lodash/array/indexOf')
+var capitalize = require('lodash/string/capitalize')
 
 var evangiles = JSON.parse(fs.readFileSync(path.resolve('public/data/evangiles.json')))
+var blackList = JSON.parse(fs.readFileSync(path.resolve('public/data/most-frequent-french-worlds.json')))
+    .map(function (w) {
+        return capitalize(w)
+    })
 
-evangiles.forEach(function(v){
-    // remove verset num
-    // split with multiples separators _ . , ' ? : ; -
-    // http://stackoverflow.com/questions/650022/how-do-i-split-a-string-with-multiple-separators-in-javascript
-    // filter string <= 1
-    // filter string with no Uppercase
-    console.log(v.text.trim().replace(/\d+:\d+ /, '').split(/(?: |'|\.|;|\?|:|-)/).filter(function(w){
-        return w.length > 1 && /[A-ZÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]/.test(w.charAt(0))
-    }))
+
+evangiles.forEach(function (v) {
+    console.log(v.text
+        .trim()
+        // remove verset num
+        .replace(/\d+:\d+ /, '')
+        // split with multiples separators _ . , ' ? : ; - !
+        // http://stackoverflow.com/questions/650022/how-do-i-split-a-string-with-multiple-separators-in-javascript
+        .split(/(?: |'|\.|,|;|\?|:|-|!)/)
+        .filter(function (w) {
+            // filter string <= 1
+            return w.length > 1
+                    // filter string with no Uppercase
+                && /[A-ZÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]/.test(w.charAt(0))
+                && indexOf(blackList, w) === -1
+
+        }))
 
 })
